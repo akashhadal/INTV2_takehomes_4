6c67d21b183f6ba19405d7a16e6720dc2c3c1b81
Akash Hadal <akash.hadal@gmail.com>
2024-06-17 10:59:53 +0530

    Spellcheck App V1.0

---
+++ xspellcheckapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SpellCheckApp from './spellCheckApp';
+
+function App() {
+  return (
+    <div className="App">
+      <SpellCheckApp />
+    </div>
+  );
+}
+
+export default App;
